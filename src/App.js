6268e845c9f22d506c2fe785c235ca042ca09764
ThatoMahloko <thato732mahloko@gmail.com>
2022-02-08 11:16:00 +0200

    add doctors add fascilities

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import './App.css';
-import React , {useEffect}from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LogIn } from './Components/LogIn';
-import {SignUp} from './Components/SignUp';
+import { SignUp } from './Components/SignUp';
 import { Dashboard } from './Components/Dashboard';
-import {Appointment} from './Components/Appointment';
-import {Doctors} from './Components/Doctors';
-import {DoctorsForm} from './Components/DoctorsForm';
-import {Patients} from './Components/Patients';
-import {getBookings} from './databaseServices/services'
+import { Appointment } from './Components/Appointment';
+import { Doctors } from './Components/Doctors';
+import { DoctorsForm } from './Components/DoctorsForm';
+import { Patients } from './Components/Patients';
+import { MedicalFascilities } from './Components/MedicalFascilities'
+import { getBookings } from './databaseServices/services'
 
 function App() {
 
-  useEffect(()=>{
+  useEffect(() => {
     getBookings()
 
   }, [])
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LogIn/>}/>
-      <Route path='SignUp' element={<SignUp/>}/>
-      <Route path='Dashboard' element={<Dashboard/>}/>
-      <Route path='Appointment' element={<Appointment/>}/>
-      <Route path='Doctors' element={<Doctors/>}/>
-      <Route path='DoctorsForm' element={<DoctorsForm/>}/>
-      <Route path='Patients' element={<Patients/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<LogIn />} />
+        <Route path='SignUp' element={<SignUp />} />
+        <Route path='Dashboard' element={<Dashboard />} />
+        <Route path='Appointment' element={<Appointment />} />
+        <Route path='Doctors' element={<Doctors />} />
+        <Route path='DoctorsForm' element={<DoctorsForm />} />
+        <Route path='Patients' element={<Patients />} />
+        <Route path='MedicalFascilities' element={<MedicalFascilities />} />
+      </Routes>
     </BrowserRouter>
   );
 }
